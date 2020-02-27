795778cb8d31a3518c6cb6f4e5ba3e6224976083
Juarez Junior <juarezjunior.live@gmail.com>
2020-02-27 18:30:53 -0300

    Projeto em andamento

--- src/app.js
+++ src/app.js
@@ -4,34 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//carregar as rotas
+const indexRotas = require('./routes/index.rotas');
+const produtosRotas = require('./routes/produtos.rotas');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "node api", version: "0.1"
-    });
-});
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+app.use('/', indexRotas);
+app.use('/produtos', produtosRotas);
 
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/produtos', create);
-app.use('/produtos', put);
-app.use('/produtos', del);
 
 module.exports = app;
